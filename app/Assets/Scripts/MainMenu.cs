72182564d2efe0adea7e7d41068163188820cd3e
Simon Hochholzer <simon.hochholzer@posteo.de>
2017-05-14 17:28:31 +0200

    #18, initiated game

---
+++ app/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void ChangeScene(string sceneName) {
+		Application.LoadLevel(sceneName);
+	}
+}
