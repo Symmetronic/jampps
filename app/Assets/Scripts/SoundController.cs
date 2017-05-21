92445a0bbe939ed18bbaf06d5d116f37d7630d70
Simon Hochholzer <simon@symmetronic.com>
2017-05-21 12:30:12 +0200

    #22, designed main menu

---
+++ app/Assets/Scripts/SoundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+
+	public void ToggleSound() {
+		if (audio.mute)
+			audio.mute = false;
+		else
+			audio.mute = true;
+	}
+}
