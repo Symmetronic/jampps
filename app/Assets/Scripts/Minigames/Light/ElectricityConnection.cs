d897f3bb762fd4acb3d31b41ed80ac2060b33bb8
Simon Hochholzer <simon@symmetronic.com>
2017-06-06 15:31:28 +0200

    basic functionality for light minigame

---
+++ app/Assets/Scripts/Minigames/Light/ElectricityConnection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityConnection : MonoBehaviour {
+
+	public GameObject OnState;
+	public GameObject OffState;
+	public bool Activated;
+
+	void Start() {
+		SetActive (Activated);
+	}
+
+	public void SetActive(bool active) {
+		if (active) {
+			SetOn ();
+		} else {
+			SetOff ();
+		}
+	}
+
+	public void ToggleActive() {
+		if (Activated) {
+			SetOff ();
+		} else {
+			SetOn ();
+		}
+	}
+
+	void SetOn() {
+		OffState.SetActive (false);
+		OnState.SetActive (true);
+		Activated = true;
+	}
+
+	void SetOff() {
+		OnState.SetActive (false);
+		OffState.SetActive (true);
+		Activated = false;
+	}
+
+}
