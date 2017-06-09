155c8cb52c8a736bc7e37f44415964a8f3733975
Simon Hochholzer <simon@symmetronic.com>
2017-06-09 11:09:47 +0200

    added credits page

---
+++ app/Assets/Scripts/BackgroundMusicController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour
+{
+    AudioSource backgroundMusic;
+
+    void Awake()
+    {
+        backgroundMusic = GetComponent<AudioSource>();
+    }
+
+    public void ToggleSound()
+    {
+        if (backgroundMusic.mute)
+            backgroundMusic.mute = false;
+        else
+            backgroundMusic.mute = true;
+    }
+}
---
+++ app/Assets/Scripts/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+    private static Singleton instance = null;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public static Singleton GetInstance
+    {
+        get { return instance; }
+    }
+
+}
