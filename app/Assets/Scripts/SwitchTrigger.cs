8578a7ab16b0249019eddc516ca64b42c9f7469d
Paul Schreiber <paulshandy96@gmail.com>
2017-06-11 16:57:23 +0200

    Add windturbine, Connect Interaction_Mother to outside_top-down

--- app/Assets/Scripts/SwitchTrigger.cs
+++ app/Assets/Scripts/SwitchTrigger.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchTrigger : MonoBehaviour {
+public bool inTrigger;
 
+
+	void Update(){
+		
+		if (Input.GetKeyDown("space")){
+			
+			if (inTrigger ){		
+			UnityEngine.SceneManagement.SceneManager.LoadScene(1);	
+			}
+		}
+		
+	}
 	
 	void OnTriggerEnter2D (Collider2D player){
 		
-		
-		
-		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
-		
-		
+		inTrigger = true;
 		
 	}
 
+	void OnTriggerExit2D (Collider2D player){
+		
+		inTrigger = false;
+		
+	}
+	
 }
