a2e423ccc36cbd5c4f8fbcbfd55b15ab362bd77e
Paul Schreiber <paulshandy96@gmail.com>
2017-06-11 15:45:23 +0200

    Connect TopDown to LightMinigame

---
+++ app/Assets/Scripts/SwitchTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTrigger : MonoBehaviour {
+
+	
+	void OnTriggerEnter2D (Collider2D player){
+		
+		
+		
+		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+		
+		
+		
+	}
+
+}
