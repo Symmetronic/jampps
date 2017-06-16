a92cb91b06d0089df88c1c0e3c730f22b9f641d9
Peter Egermann <s2236306@mail.zih.tu-dresden.de>
2017-06-16 16:05:47 +0200

    Add trigger for next scene in scene Chapter2_Top-Down_Outside

---
+++ app/Assets/Scripts/change_scene_without_space.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_scene_without_space: MonoBehaviour
+{
+    public int scene;
+
+   
+    void OnTriggerEnter2D(Collider2D player)
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+
+    }
+
+}
