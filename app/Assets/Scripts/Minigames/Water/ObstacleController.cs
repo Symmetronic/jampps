47cfea5c639dc80bd882e41f9d6b9aba2d9bc915
Simon Hochholzer <simon@symmetronic.com>
2017-06-23 10:52:13 +0200

    #39, init water minigame

---
+++ app/Assets/Scripts/Minigames/Water/ObstacleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+    public bool moveHorizontal = true;
+    public float gridUnit = 67;
+    
+    public void Move()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 objPos = transform.position;
+        Vector3 transformVector = new Vector3();
+        
+        if (moveHorizontal)
+        {
+            if (mousePos.x < objPos.x)
+                transformVector.Set(-1 * gridUnit, 0, 0);
+            else
+                transformVector.Set(gridUnit, 0, 0);
+        }
+        else
+        {
+            if (mousePos.y < objPos.y)
+                transformVector.Set(gridUnit, 0, 0);
+            else
+                transformVector.Set(-1 * gridUnit, 0, 0);
+        }
+
+        transform.Translate(transformVector);
+    }
+
+}
