dc0c03794b5dbf814dab1110c106c91177afb92f
Simon Hochholzer <simon@symmetronic.com>
2017-06-24 12:38:41 +0200

    refine light minigame

--- app/Assets/Scripts/Minigames/Light/LightGameController.cs
+++ app/Assets/Scripts/Minigames/Light/LightGameController.cs
@@ -6,9 +6,11 @@ public class LightGameController : MonoBehaviour {
 
     public GameObject[] lightBulbs;
     public string nextScene;
+    public string minigameName;
     public AudioSource failureAudio;
     public GameObject errorMessage;
     public GameObject karsten;
+    private int score = 0;
 
     private bool AllLightBulbsOff()
     {
@@ -23,11 +25,13 @@ public class LightGameController : MonoBehaviour {
 
     private void EndGame()
     {
+        GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreController>().AddScore(minigameName, score);
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
     }
 
 	public void LightBulbChanged (bool notifyFailure)
     {
+        RaiseScore();
         if (notifyFailure)
         {
             failureAudio.Play();
@@ -42,6 +46,11 @@ public class LightGameController : MonoBehaviour {
         VisibilityController visibilityController = errorMessage.GetComponent<VisibilityController>();
         visibilityController.Show(errorMessage);
         visibilityController.Show(karsten);
+    }
+
+    private void RaiseScore()
+    {
+        score++;
     }
 
 
