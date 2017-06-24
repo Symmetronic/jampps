eb9fd89c056351788ae25a7ed04e4f7cc8192b4b
Simon Hochholzer <simon@symmetronic.com>
2017-06-24 12:40:29 +0200

    add first version of water minigame

--- app/Assets/Scripts/Minigames/Water/ObstacleController.cs
+++ app/Assets/Scripts/Minigames/Water/ObstacleController.cs
@@ -5,30 +5,64 @@ using UnityEngine;
 public class ObstacleController : MonoBehaviour {
 
     public bool moveHorizontal = true;
-    public float gridUnit = 67;
+    public Vector2 position;
+    public int size;
+    private WaterMinigameController gameController;
+
+    public void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<WaterMinigameController>();
+    }
     
     public void Move()
     {
         Vector2 mousePos = Input.mousePosition;
         Vector2 objPos = transform.position;
-        Vector3 transformVector = new Vector3();
+        Vector3 transformVector = new Vector3(0, 0);
         
         if (moveHorizontal)
         {
             if (mousePos.x < objPos.x)
-                transformVector.Set(-1 * gridUnit, 0, 0);
+            {
+                if (gameController.IsEmpty(new Vector2(position.x - 1, position.y)))
+                {
+                    transformVector.Set(-1 * gameController.gridUnit, 0, 0);
+                    position = new Vector2(position.x - 1, position.y);
+                }
+            }
             else
-                transformVector.Set(gridUnit, 0, 0);
+            {
+                if (gameController.IsEmpty(new Vector2(position.x + size, position.y)))
+                {
+                    transformVector.Set(gameController.gridUnit, 0, 0);
+                    position = new Vector2(position.x + 1, position.y);
+                }
+            }
         }
         else
         {
             if (mousePos.y < objPos.y)
-                transformVector.Set(gridUnit, 0, 0);
+            {
+                if (gameController.IsEmpty(new Vector2(position.x, position.y + size)))
+                {
+                    transformVector.Set(gameController.gridUnit, 0, 0);
+                    position = new Vector2(position.x, position.y + 1);
+                }
+            }
             else
-                transformVector.Set(-1 * gridUnit, 0, 0);
+            {
+                if (gameController.IsEmpty(new Vector2(position.x, position.y - 1)))
+                {
+                    transformVector.Set(-1 * gameController.gridUnit, 0, 0);
+                    position = new Vector2(position.x, position.y - 1);
+                }
+            }
         }
-
-        transform.Translate(transformVector);
+        if (transformVector != new Vector3(0, 0))
+        {
+            transform.Translate(transformVector);
+            gameController.RaiseScore();
+        }
     }
 
 }
--- app/Assets/Scripts/Minigames/Water/WaterMinigameController.cs
+++ app/Assets/Scripts/Minigames/Water/WaterMinigameController.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class WaterMinigameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string nextScene;
+    public string minigameName;
+    public int fieldSize;
+    public float gridUnit = 67;
+    public GameObject[] obstacles;
+    private int score = 0;
+    
+    public bool IsEmpty(Vector2 position)
+    {
+        if ((position.x < 1 && position.y != 3) || position.x < -1 || position.y < 1 || (position.x > fieldSize && position.y != 3) || position.x > (fieldSize + 2) || position.y > 5)
+            return false;
+        else
+        {
+            foreach(GameObject obstacle in obstacles)
+            {
+                ObstacleController obstCtrl = obstacle.GetComponent<ObstacleController>();
+                for (int i = 0; i < obstCtrl.size; i++) {
+                    Vector2 currPos = new Vector2();
+                    if (obstCtrl.moveHorizontal)
+                        currPos.Set(obstCtrl.position.x + i, obstCtrl.position.y);
+                    else
+                        currPos.Set(obstCtrl.position.x, obstCtrl.position.y + i);
+                    if (currPos == position)
+                        return false;
+                }
+            }
+            if (position.x == -1 && position.y == 3)
+                EndGame();
+            return true;
+        }
+    }
+
+    private void EndGame()
+    {
+        GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreController>().AddScore(minigameName, score);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+    }
+
+    public void RaiseScore()
+    {
+        score++;
+    }
 }
