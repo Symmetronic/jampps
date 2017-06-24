873e732490a805712eb2bd24dba5a8067b6e2993
Simon Hochholzer <simon@symmetronic.com>
2017-06-24 12:36:59 +0200

    add logic to end chapter 1

---
+++ app/Assets/Scripts/Minigames/FeedbackController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackController : MonoBehaviour {
+
+    public string nextScene;
+    public string chapterCompleteScene = "";
+    public int chapter;
+
+    public void GoToNextScene()
+    {
+        MenuController menuCtrl = GetComponent<MenuController>();
+        GameObject score = GameObject.FindGameObjectWithTag("Score");
+        ScoreController scoreCtrl = score.GetComponent<ScoreController>();
+        if (chapterCompleteScene != "" && score && scoreCtrl.ChapterComplete(chapter))
+        {
+            menuCtrl.ChangeScene(chapterCompleteScene);
+        }
+        else
+        {
+            menuCtrl.ChangeScene(nextScene);
+        }
+    }
+}
