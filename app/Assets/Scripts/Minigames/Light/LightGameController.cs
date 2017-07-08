6ca389d7b30067239acac7970fba5a6c0326cef7
Simon Hochholzer <simon@symmetronic.com>
2017-07-08 16:11:59 +0200

    adjust volumes, fix audio problems

--- app/Assets/Scripts/Minigames/Light/LightGameController.cs
+++ app/Assets/Scripts/Minigames/Light/LightGameController.cs
@@ -7,12 +7,11 @@ public class LightGameController : MonoBehaviour {
     public GameObject[] lightBulbs;
     public string nextScene;
     public string minigameName;
-    public AudioSource failureAudio;
     public GameObject errorMessage;
     public GameObject karsten;
     private int score = 0;
-    
-   
+
+
 
     private bool AllLightBulbsOff()
     {
@@ -27,7 +26,7 @@ public class LightGameController : MonoBehaviour {
 
     private void EndGame()
     {
-        
+
         GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreController>().AddScore(minigameName, score);
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
     }
@@ -37,7 +36,6 @@ public class LightGameController : MonoBehaviour {
         RaiseScore();
         if (notifyFailure)
         {
-            failureAudio.Play();
             ShowErrorMessage();
         } else if (AllLightBulbsOff()) {
             EndGame();
